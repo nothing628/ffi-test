fd20a145232653dc5deb8fd7315ff09b840d0b7b
yogyphang <yogyphang@gmail.com>
2024-05-02 22:15:03 +0700

    create subchunk method

--- src/file_joiner.rs
+++ src/file_joiner.rs
@@ -16,7 +16,7 @@ fn le_to_u32(inp: &[u8]) -> u32 {
         2 => {
             let mut out: u32 = inp[0].into();
             out = (inp[1] as u32) << 8 | out;
-            
+
             return out;
         }
         3 => {
@@ -25,7 +25,7 @@ fn le_to_u32(inp: &[u8]) -> u32 {
             out = (inp[2] as u32) << 16 | out;
 
             return out;
-        },
+        }
         4 => {
             let mut out: u32 = inp[0].into();
             out = (inp[1] as u32) << 8 | out;
@@ -40,9 +40,33 @@ fn le_to_u32(inp: &[u8]) -> u32 {
     output
 }
 
-pub fn join_webp(inp: &Vec<u8>, target: &Vec<u8>) -> Result<Vec<u8>, JoinError> {
+fn usize_to_le(inp: usize) -> [u8; 4] {
+    let mut bytes = [0u8; 4];
+
+    bytes[0] = (inp & 0xFF) as u8;
+    bytes[1] = ((inp >> 8) & 0xFF) as u8;
+    bytes[2] = ((inp >> 16) & 0xFF) as u8;
+    bytes[3] = ((inp >> 24) & 0xFF) as u8;
+
+    bytes
+}
+
+fn create_subchunk(chunk_id: &str, data: &[u8]) -> Vec<u8> {
+    let chunk_size = data.len();
+    let chunk_size_bytes = usize_to_le(chunk_size);
+    let chunk_id_bytes = chunk_id.as_bytes();
+    let mut result = Vec::from(chunk_id_bytes);
+
+    chunk_size_bytes.iter().for_each(|f| result.push(*f));
+    data.iter().for_each(|f| result.push(*f));
+
+    result
+}
+
+pub fn join_webp(inp: &[u8], target: &[u8]) -> Result<Vec<u8>, JoinError> {
     let result = Vec::new();
     let size_slice = inp.get(4..8).ok_or_else(|| JoinError::InvalidWebpFile)?;
+    let chunk = create_subchunk("milf", &target[..]);
     let size = le_to_u32(&size_slice);
 
     Ok(result)
