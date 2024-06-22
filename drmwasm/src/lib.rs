68184283cded6ddac83508883b4f3c35c9e9a6a9
nothing628 <yogyphang@gmail.com>
2024-06-22 14:24:22 +0700

    add struct replacementImage

--- drmwasm/src/lib.rs
+++ drmwasm/src/lib.rs
@@ -2,7 +2,24 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
+
 #[wasm_bindgen]
 pub fn add(x: u32) -> u32 {
     x + 2
+}
+
+#[wasm_bindgen]
+pub struct ReplacementImage {
+    x: u32,
+    y: u32,
+    real_img: Vec<u8>,
+}
+
+#[wasm_bindgen]
+pub fn get_replacement_img(inp_bytes: Vec<u8>) -> ReplacementImage {
+    ReplacementImage {
+        x: 12,
+        y: 32,
+        real_img: Vec::new(),
+    }
 }
