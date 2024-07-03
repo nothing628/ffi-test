1b52770e3b7eb002d1fb40600e4b861f6ec1f8b7
nothing628 <yogyphang@gmail.com>
2024-07-03 12:02:00 +0700

    add file input in drmweb for test purpose

--- drmwasm/src/lib.rs
+++ drmwasm/src/lib.rs
@@ -1,5 +1,6 @@
 mod utils;
 
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -8,13 +9,24 @@ pub fn add(x: u32) -> u32 {
 }
 
 #[wasm_bindgen]
+#[derive(Serialize, Deserialize)]
 pub struct ReplacementImage {
     pub x: u32,
     pub y: u32,
+    pub width: u32,
+    pub height: u32,
     real_img: Vec<u8>,
 }
 
 #[wasm_bindgen]
-pub fn get_replacement_img(inp_bytes: Vec<u8>) -> Vec<u8> {
-    Vec::from([1,2,4])
+pub fn get_replacement_img(inp_bytes: Vec<u8>) -> Result<JsValue, JsValue>{
+    let replacement = ReplacementImage {
+        real_img: Vec::from([29, 124, 55]),
+        x: 12,
+        y: 41,
+        height: 123,
+        width: 400,
+    };
+
+    Ok(serde_wasm_bindgen::to_value(&replacement)?)
 }
--- drmweb/src/App.vue
+++ drmweb/src/App.vue
@@ -1,37 +1,74 @@
 <script setup lang="ts">
-import { onMounted } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { onMounted, ref } from "vue";
 import { add, get_replacement_img } from "drmwasm";
+
+const image = ref();
+
+const readFile = async (file: File) => {
+  const promise = new Promise<ArrayBuffer>((resolve, reject) => {
+    const file_reader = new FileReader();
+    file_reader.onload = () => {
+      const file_content = file_reader.result;
+
+      if (file_content) {
+        resolve(file_content as ArrayBuffer);
+      } else {
+        reject("Cannot read file content");
+      }
+    };
+    file_reader.readAsArrayBuffer(file);
+  });
+
+  return promise;
+};
+
+const drawImage = (file_content: Uint8Array) => {
+  if (image.value) {
+    const image_elem = image.value as HTMLImageElement;
+    const file_blob = new Blob([file_content], { type: "image/jpeg" });
+    const file_url = URL.createObjectURL(file_blob);
+
+    image_elem.src = file_url;
+  }
+};
+
+const handleFile = async (event: Event) => {
+  const target = event.target as HTMLInputElement;
+  const files = target.files;
+
+  if (files && files.length > 0) {
+    const first_file = files.item(0);
+
+    if (first_file) {
+      const file_content = await readFile(first_file);
+      const file_uint8 = new Uint8Array(file_content);
+      const subimage_replacement = get_replacement_img(file_uint8);
+
+      console.log(subimage_replacement);
+      drawImage(file_uint8);
+    }
+  }
+};
 
 onMounted(() => {
   console.log(add);
   console.log(add(20));
-  console.log(get_replacement_img([9, 2, 9]));
-})
+  console.log(get_replacement_img(new Uint8Array()));
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div>
+      <input type="file" accept="image/*" @change="handleFile" />
+      <img class="preview" ref="image" />
+    </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.preview {
+  width: auto;
+  height: 800px;
 }
 </style>
