5f49268f2e56d6c349629f2258ed946f202a6cd8
nothing628 <yogyphang@gmail.com>
2024-09-04 09:19:58 +0700

    add more macro

--- drmlib/src/macro_helper.rs
+++ drmlib/src/macro_helper.rs
@@ -6,22 +6,121 @@ macro_rules! create_get_old_section_func {
             let watermark_task = unsafe { &mut *ptr };
             let target_arr = unsafe { &mut *target };
             let output = watermark_task.get_old_section();
-        
+
             if let Some(output_img) = output {
                 let mut bytes: Vec<u8> = Vec::new();
                 let mut cur = Cursor::new(&mut bytes);
                 let output_bin = output_img.write_to(&mut cur, $export);
-        
+
                 if let Err(_) = output_bin {
                     return 2;
                 }
-        
+
                 target_arr.arr = bytes;
-        
+
                 return 0;
             }
-        
+
             1
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! create_get_output_func {
+    ($t:ident, $u:ident, $v:ident, $export:expr) => {
+        #[wasm_bindgen]
+        pub fn $t(ptr: *mut WatermarkTask, target: *mut ArrResult) -> u32 {
+            let watermark_task = unsafe { &mut *ptr };
+            let target_arr = unsafe { &mut *target };
+            return $u(watermark_task, target_arr);
+        }
+
+        fn $u(watermark_task: &mut WatermarkTask, target_arr: &mut ArrResult) -> u32 {
+            let output = watermark_task.get_output();
+            let old_section = watermark_task.get_old_section();
+            let mut bytes: Vec<u8> = Vec::new();
+            let mut old_bytes: Vec<u8> = Vec::new();
+
+            if let Some(output_img) = output {
+                let mut cur = Cursor::new(&mut bytes);
+                let output_bin = output_img.write_to(&mut cur, $export);
+
+                if let Err(_) = output_bin {
+                    return 2;
+                }
+            } else {
+                return 1;
+            }
+
+            if let Some(old_img) = old_section {
+                let mut cur_old = Cursor::new(&mut old_bytes);
+                let output_old = old_img.write_to(&mut cur_old, $export);
+
+                if let Err(_) = output_old {
+                    return 3;
+                }
+            } else {
+                return 1;
+            }
+
+            if let None = watermark_task.get_key() {
+                return 4;
+            }
+
+            let watermark_pos: [u8; 8] = watermark_task
+                .get_absolute_watermark_position()
+                .unwrap()
+                .into();
+            let watermark_dim: [u8; 8] = watermark_task.get_watermark_dimension().unwrap().into();
+            old_bytes.extend(watermark_pos);
+            old_bytes.extend(watermark_dim);
+
+            let enc_key = watermark_task.get_key().unwrap();
+            let join_result = $v(&bytes, &old_bytes, &enc_key);
+            if let Ok(result) = join_result {
+                target_arr.arr = result;
+
+                return 0;
+            }
+
+            5
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! create_set_watermark_func {
+    ($t:ident,$typ:expr) => {
+        #[wasm_bindgen]
+        pub fn $t(ptr: *mut WatermarkTask, byts_ptr: *const u8, byts_len: usize) -> u32 {
+            let byts = unsafe { std::slice::from_raw_parts(byts_ptr, byts_len) };
+            let watermark_task = unsafe { &mut *ptr };
+
+            if let Err(_) = set_watermark(watermark_task, byts, $typ) {
+                return 1;
+            }
+            0
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! create_set_target_func {
+    ($t:ident,$typ:expr) => {
+        #[wasm_bindgen]
+        pub fn $t(
+            ptr: *mut WatermarkTask,
+            byts_ptr: *const u8,
+            byts_len: usize,
+        ) -> u32 {
+            let byts = unsafe { std::slice::from_raw_parts(byts_ptr, byts_len) };
+            let watermark_task = unsafe { &mut *ptr };
+
+            if let Err(_) = set_target(watermark_task, byts, $typ) {
+                return 1;
+            }
+            0
         }
     };
 }
--- drmlib/src/watermark.rs
+++ drmlib/src/watermark.rs
@@ -7,7 +7,10 @@ use drmcore::watermark_task::{set_target, set_watermark, OriginX, OriginY, Water
 use wasm_bindgen::prelude::*;
 
 use crate::arr_result::ArrResult;
-use crate::create_get_old_section_func;
+use crate::{
+    create_get_old_section_func, create_get_output_func, create_set_target_func,
+    create_set_watermark_func,
+};
 
 #[wasm_bindgen]
 pub fn create_watermarktask() -> *mut WatermarkTask {
@@ -18,13 +21,7 @@ pub fn create_watermarktask() -> *mut WatermarkTask {
 }
 
 #[wasm_bindgen]
-pub fn set_position_watermark(
-    ptr: *mut WatermarkTask,
-    x: u32,
-    y: u32,
-    origin_x: u8,
-    origin_y: u8,
-) {
+pub fn set_position_watermark(ptr: *mut WatermarkTask, x: u32, y: u32, origin_x: u8, origin_y: u8) {
     let watermark_task = unsafe { &mut *ptr };
     let real_origin_x = if origin_x == 0 {
         OriginX::Left
@@ -37,66 +34,6 @@ pub fn set_position_watermark(
         OriginY::Bottom
     };
     watermark_task.set_position(x, y, real_origin_x, real_origin_y);
-}
-
-#[wasm_bindgen]
-pub fn set_target_webp(
-    ptr: *mut WatermarkTask,
-    byts_ptr: *const u8,
-    byts_len: usize,
-) -> u32 {
-    let byts = unsafe { std::slice::from_raw_parts(byts_ptr, byts_len) };
-    let watermark_task = unsafe { &mut *ptr };
-
-    if let Err(_) = set_target(watermark_task, byts, ImageFormat::WebP) {
-        return 1;
-    }
-    0
-}
-
-#[wasm_bindgen]
-pub fn set_target_jpeg(
-    ptr: *mut WatermarkTask,
-    byts_ptr: *const u8,
-    byts_len: usize,
-) -> u32 {
-    let byts = unsafe { std::slice::from_raw_parts(byts_ptr, byts_len) };
-    let watermark_task = unsafe { &mut *ptr };
-
-    if let Err(_) = set_target(watermark_task, byts, ImageFormat::Jpeg) {
-        return 1;
-    }
-    0
-}
-
-#[wasm_bindgen]
-pub fn set_watermark_webp(
-    ptr: *mut WatermarkTask,
-    byts_ptr: *const u8,
-    byts_len: usize,
-) -> u32 {
-    let byts = unsafe { std::slice::from_raw_parts(byts_ptr, byts_len) };
-    let watermark_task = unsafe { &mut *ptr };
-
-    if let Err(_) = set_watermark(watermark_task, byts, ImageFormat::WebP) {
-        return 1;
-    }
-    0
-}
-
-#[wasm_bindgen]
-pub fn set_watermark_jpeg(
-    ptr: *mut WatermarkTask,
-    byts_ptr: *const u8,
-    byts_len: usize,
-) -> u32 {
-    let byts = unsafe { std::slice::from_raw_parts(byts_ptr, byts_len) };
-    let watermark_task = unsafe { &mut *ptr };
-
-    if let Err(_) = set_watermark(watermark_task, byts, ImageFormat::Jpeg) {
-        return 1;
-    }
-    0
 }
 
 #[wasm_bindgen]
@@ -128,124 +65,14 @@ pub fn process_watermark(ptr: *mut WatermarkTask) -> u32 {
     0
 }
 
-create_get_old_section_func!{get_old_section_jpeg,ImageFormat::Jpeg}
-create_get_old_section_func!{get_old_section_webp,ImageFormat::WebP}
-
-#[wasm_bindgen]
-pub fn get_output_webp(ptr: *mut WatermarkTask, target: *mut ArrResult) -> u32 {
-    let watermark_task = unsafe { &mut *ptr };
-    let target_arr = unsafe { &mut *target };
-    return get_output_webp_native(watermark_task, target_arr);
-}
-
-#[wasm_bindgen]
-pub fn get_output_jpeg(ptr: *mut WatermarkTask, target: *mut ArrResult) -> u32 {
-    let watermark_task = unsafe { &mut *ptr };
-    let target_arr = unsafe { &mut *target };
-    return get_output_jpeg_native(watermark_task, target_arr);
-}
-
-fn get_output_webp_native(watermark_task: &mut WatermarkTask, target_arr: &mut ArrResult) -> u32 {
-    let output = watermark_task.get_output();
-    let old_section = watermark_task.get_old_section();
-    let mut bytes: Vec<u8> = Vec::new();
-    let mut old_bytes: Vec<u8> = Vec::new();
-
-    if let Some(output_img) = output {
-        let mut cur = Cursor::new(&mut bytes);
-        let output_bin = output_img.write_to(&mut cur, ImageFormat::WebP);
-
-        if let Err(_) = output_bin {
-            return 2;
-        }
-    } else {
-        return 1;
-    }
-
-    if let Some(old_img) = old_section {
-        let mut cur_old = Cursor::new(&mut old_bytes);
-        let output_old = old_img.write_to(&mut cur_old, ImageFormat::WebP);
-
-        if let Err(_) = output_old {
-            return 3;
-        }
-    } else {
-        return 1;
-    }
-
-    if let None = watermark_task.get_key() {
-        return 4;
-    }
-
-    let watermark_pos: [u8; 8] = watermark_task
-        .get_absolute_watermark_position()
-        .unwrap()
-        .into();
-    let watermark_dim: [u8; 8] = watermark_task.get_watermark_dimension().unwrap().into();
-    old_bytes.extend(watermark_pos);
-    old_bytes.extend(watermark_dim);
-
-    let enc_key = watermark_task.get_key().unwrap();
-    let join_result = join_webp(&bytes, &old_bytes, &enc_key);
-    if let Ok(result) = join_result {
-        target_arr.arr = result;
-
-        return 0;
-    }
-
-    5
-}
-
-fn get_output_jpeg_native(watermark_task: &mut WatermarkTask, target_arr: &mut ArrResult) -> u32 {
-    let output = watermark_task.get_output();
-    let old_section = watermark_task.get_old_section();
-    let mut bytes: Vec<u8> = Vec::new();
-    let mut old_bytes: Vec<u8> = Vec::new();
-
-    if let Some(output_img) = output {
-        let mut cur = Cursor::new(&mut bytes);
-        let output_bin = output_img.write_to(&mut cur, ImageFormat::Jpeg);
-
-        if let Err(_) = output_bin {
-            return 2;
-        }
-    } else {
-        return 1;
-    }
-
-    if let Some(old_img) = old_section {
-        let mut cur_old = Cursor::new(&mut old_bytes);
-        let output_old = old_img.write_to(&mut cur_old, ImageFormat::Jpeg);
-
-        if let Err(_) = output_old {
-            return 3;
-        }
-    } else {
-        return 1;
-    }
-
-    if let None = watermark_task.get_key() {
-        return 4;
-    }
-
-    let watermark_pos: [u8; 8] = watermark_task
-        .get_absolute_watermark_position()
-        .unwrap()
-        .into();
-    let watermark_dim: [u8; 8] = watermark_task.get_watermark_dimension().unwrap().into();
-    old_bytes.extend(watermark_pos);
-    old_bytes.extend(watermark_dim);
-
-    let enc_key = watermark_task.get_key().unwrap();
-    let join_result = join_jpeg(&bytes, &old_bytes, &enc_key);
-    if let Ok(result) = join_result {
-        target_arr.arr = result;
-
-        return 0;
-    }
-
-    5
-}
+create_set_target_func! {set_target_webp,ImageFormat::WebP}
+create_set_target_func! {set_target_jpeg,ImageFormat::Jpeg}
+create_set_watermark_func! {set_watermark_webp,ImageFormat::WebP}
+create_set_watermark_func! {set_watermark_jpeg,ImageFormat::Jpeg}
+create_get_old_section_func! {get_old_section_jpeg,ImageFormat::Jpeg}
+create_get_old_section_func! {get_old_section_webp,ImageFormat::WebP}
+create_get_output_func! {get_output_jpeg,get_output_jpeg_native,join_jpeg,ImageFormat::Jpeg}
+create_get_output_func! {get_output_webp,get_output_webp_native,join_webp,ImageFormat::WebP}
 
 #[cfg(test)]
 mod tests {
